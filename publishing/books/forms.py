7c034e3b799e0eadfb1ce081d2ba0ee5e0be3ee2
Phil Gyford <phil@gyford.com>
2019-02-23 14:48:08 +0000

    Add comments and make some things a bit more verbose

--- publishing/books/forms.py
+++ publishing/books/forms.py
@@ -46,6 +46,11 @@ class BaseBooksWithImagesFormset(BaseInlineFormSet):
         return result
 
     def clean(self):
+        """
+        If a parent form has no data, but its nested forms do, we should
+        return an error, because we can't save the parent.
+        For example, if the Book form is empty, but there are Images.
+        """
         super().clean()
 
         for form in self.forms:
@@ -55,9 +60,9 @@ class BaseBooksWithImagesFormset(BaseInlineFormSet):
             if self._is_adding_nested_inlines_to_empty_form(form):
                 form.add_error(
                     field=None,
-                    error=_('You are trying to add Images to a Book which '
+                    error=_('You are trying to add image(s) to a book which '
                             'does not yet exist. Please add information '
-                            'about the Book.'))
+                            'about the book and choose the image file(s) again.'))
 
     def save(self, commit=True):
         """
@@ -73,16 +78,30 @@ class BaseBooksWithImagesFormset(BaseInlineFormSet):
         return result
 
     def _is_adding_nested_inlines_to_empty_form(self, form):
+        """
+        Are we trying to add data in nested inlines to a form that has no data?
+        e.g. Adding Images to a new Book whose data we haven't entered?
+        """
         if not hasattr(form, 'nested'):
+            # A basic form; it has no nested forms to check.
             return False
 
-        if is_form_persisted(form) or not is_empty_form(form):
+        if is_form_persisted(form):
+            # We're editing (not adding) an existing model.
+            return False
+            
+        if not is_empty_form(form):
+            # The form has errors, or it contains valid data.
             return False
 
-        non_deleted_forms = (set(form.nested.forms)
-                             .difference(set(form.nested.deleted_forms)))
-        return any(not is_empty_form(nested_form)
-                   for nested_form in non_deleted_forms)
+        # All the inline forms that aren't being deleted:
+        non_deleted_forms = set(form.nested.forms).difference(
+            set(form.nested.deleted_forms)
+        )
+
+        # In all the inline forms that aren't being deleted, are there any that
+        # contain data? Return True if so.
+        return any(not is_empty_form(nested_form) for nested_form in non_deleted_forms)
 
 
 # This is the formset for the Books belonging to a Publisher and the
