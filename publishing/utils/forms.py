77c32db801bb8146acc0d10d06918bc6ff7e55b7
Phil Gyford <phil@gyford.com>
2019-02-23 14:48:08 +0000

    Add comments and make some things a bit more verbose

--- publishing/utils/forms.py
+++ publishing/utils/forms.py
@@ -1,6 +1,23 @@
 def is_empty_form(form):
-    return form.is_valid() and not form.cleaned_data
+    """
+    Is this form valid (has no errors) and contains no data?
+    """
+    if form.is_valid() and not form.cleaned_data:
+        return True
+    else:
+        # Either the form has errors (isn't valid) or
+        # it doesn't have errors and contains data.
+        return False
 
 
 def is_form_persisted(form):
-    return form.instance and not form.instance._state.adding
+    """
+    Does the form have a model instance attached and it's not being added?
+    e.g. The form is about an existing Book whose data is being edited.
+    """
+    if form.instance and not form.instance._state.adding:
+        return True
+    else:
+        # Either the form has no instance attached or
+        # it has an instance that is being added.
+        return False
