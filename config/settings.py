48ca146785738bac0054a685c13df6e4e4c76259
Phil Gyford <phil@gyford.com>
2022-02-20 18:15:17 +0000

    Move templates into a separate project-level directory

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "publishing", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
